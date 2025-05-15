1a7ccd48f04d185ee9019eb2cb481f124f7b6b62
tramcandoit <tramngocbtnt@gmail.com>
2025-05-15 15:18:08 +0700

    feat: add noti to slack

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# iot-smart-lock
+## User guide
+### Register face
+```bash
+python register-face.py --path-to-image "karina.jpeg" --name "Karina"
+```
+
+### Detect
+```bash
+python test-detect.py
+```
--- test-detect.py
+++ test-detect.py
@@ -1,6 +1,9 @@
 import boto3
 import io
 from PIL import Image
+import os
+from slack_sdk import WebClient
+from dotenv import load_dotenv, dotenv_values 
 
 # Initialize AWS clients
 rekognition = boto3.client('rekognition', region_name='ap-southeast-1')
@@ -16,8 +19,7 @@ def preprocess_image(image_path):
     image.save(stream, format="JPEG")
     return stream.getvalue()
 
-def check_face_exists(image_binary):
-    # Send image to Rekognition and check if face exists in collection
+def check_face_exists(image_binary, slack_message_container):
     print("Searching for faces in the collection...")
     response = rekognition.search_faces_by_image(
         CollectionId=COLLECTION_ID,
@@ -32,14 +34,33 @@ def check_face_exists(image_binary):
         face_id = response['FaceMatches'][0]['Face']['FaceId']
         confidence = response['FaceMatches'][0]['Face']['Confidence']
         print(f"Face matched! FaceId: {face_id} with {confidence}% confidence.")
+        slack_message_container['message'] = f"Unlocked successfully!!!"
         return True, face_id, confidence
     else:
         print("No matching face found.")
+        slack_message_container['message'] = "Warning: Someone tried to unlock your locker!!!!"
         return False, None, None
 
+
 def main():
-    image_binary = preprocess_image("karina-test.jpg")
-    check_face_exists(image_binary)
+    image_binary = preprocess_image("gdragon.jpg")
+    
+    slack_message = {'message': ''}
+    check_face_exists(image_binary, slack_message)
+    
+    message_string = slack_message['message']
+
+    ## Slack notifications
+    load_dotenv("./.env")
+    slack_token = os.getenv("SLACK_TOKEN") 
+    
+    client = WebClient(token=slack_token)
+    client.chat_postMessage(
+        channel="locker-notifications", 
+        text=message_string, 
+        username="Locker Buddy"
+    )
+    print("Message sent to Slack!")
 
 if __name__ == "__main__":
     main()
